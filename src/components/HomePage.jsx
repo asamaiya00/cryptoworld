4ba627a1d9ed697e77e62e7d67c0870205fe7b01
Animesh Samaiya <asamaiya00@gmail.com>
2021-11-03 11:14:52 +0530

    feat: Homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,28 @@
-const HomePage = () => {
-    return (
-        <div>
-            Homepage
-        </div>
-    )
-}
+import { Col, Row, Statistic, Typography } from "antd";
 
-export default HomePage
+const HomePage = () => {
+  return (
+    <div>
+      <Typography.Title className='heading' level="2">Global Crypto Stats</Typography.Title>
+      <Row>
+        <Col span="12">
+          <Statistic title="Total Cryptocurrencies" value={"5"}></Statistic>
+        </Col>
+        <Col span="12">
+          <Statistic title="Total Exchanges" value={"5"}></Statistic>
+        </Col>
+        <Col span="12">
+          <Statistic title="Total Market Cap" value={"5"}></Statistic>
+        </Col>
+        <Col span="12">
+          <Statistic title="Total 24h Volume" value={"5"}></Statistic>
+        </Col>
+        <Col span="12">
+          <Statistic title="Total Markets" value={"5"}></Statistic>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default HomePage;
